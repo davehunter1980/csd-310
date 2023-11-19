2d658642b3fa124220c06a80f8a877129acb6f0d
Dave Hunter <pdavidhunter@gmail.com>
2023-11-19 00:52:04 -0500

    Update mysql_test.py

--- module_6/mysql_test.py
+++ module_6/mysql_test.py
@@ -8,3 +8,23 @@ config = {
     "database": "movies",
     "raise_on_warnings": True
 }
+
+try:
+    db = mysql.connector.connect(**config)
+
+    print("\n Database user {} to connected to MySQL on host {} with database {}".format(config["user"], config["host"], config["database"]))
+
+    input("\n\n Press any key to continue...")
+
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("   The supplied username and password are invalid")
+
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("   The specified database does not exist")
+
+    else:
+        print(err)
+
+finally:
+    db.close()
